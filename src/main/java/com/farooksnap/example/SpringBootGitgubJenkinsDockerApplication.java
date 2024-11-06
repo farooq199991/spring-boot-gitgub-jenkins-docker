92d1067794bdb657e84c5b74612cd95c3261b95c
farooq199991 <farooqmoghal.m@gmail.com>
2024-11-06 18:14:32 +0530

    first commite

---
+++ src/main/java/com/farooksnap/example/SpringBootGitgubJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.farooksnap.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitgubJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitgubJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farooksnap/example/SpringBootGitgubJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farooksnap.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitgubJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
