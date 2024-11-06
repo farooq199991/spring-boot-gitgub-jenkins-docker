d9ca789a8764dd83ee3b8c176c34b92cba684742
farooq199991 <farooqmoghal.m@gmail.com>
2024-11-06 18:31:34 +0530

    second commit

--- src/main/java/com/farooksnap/example/SpringBootGitgubJenkinsDockerApplication.java
+++ src/main/java/com/farooksnap/example/SpringBootGitgubJenkinsDockerApplication.java
@@ -2,9 +2,18 @@ package com.farooksnap.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootGitgubJenkinsDockerApplication {
+
+	@GetMapping("/message")
+	public String getmessage(){
+
+		return "Running the Docker for jenkins";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGitgubJenkinsDockerApplication.class, args);
